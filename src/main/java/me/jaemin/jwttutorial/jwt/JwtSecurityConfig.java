4cd14de55cc292cf197f0c4f2009345d5d5ba8a9
jaeminLee524 <rkwoals524@hanmail.net>
2022-01-15 03:46:40 +0900

    jwt 토큰 설정 생성

---
+++ src/main/java/me/jaemin/jwttutorial/jwt/JwtSecurityConfig.java
@@ -0,0 +1,23 @@
+package me.jaemin.jwttutorial.jwt;
+
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+// TOKenProvider, JwtFilter를 SecurityConfig에 적용할때 사용할 클래스
+public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    private TokenProvider tokenProvider;
+
+    public JwtSecurityConfig(TokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Override
+    public void configure(HttpSecurity http) {
+        JwtFilter customFilter = new JwtFilter(tokenProvider);
+        // jwt필터를 시큐리티로직테 필터를 등록
+        http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
