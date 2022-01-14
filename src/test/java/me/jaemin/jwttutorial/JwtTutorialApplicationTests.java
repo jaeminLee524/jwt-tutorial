a57650c8997d7db5e75ef49b25361788b21c39e8
jaeminLee524 <rkwoals524@hanmail.net>
2022-01-15 03:09:42 +0900

    Initial commit

---
+++ src/test/java/me/jaemin/jwttutorial/JwtTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jaemin.jwttutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
